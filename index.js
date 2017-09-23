c535d889e9c6f70fc814c0c4a03b83b640188832
DANIEL CHAN <dchan513@msn.com>
2017-09-23 02:46:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -133,7 +133,7 @@ function createRock(x) {
  */
 function endGame() {
   clearInterval(gameInterval)
-  //ROCKS.length = 0
+  ROCKS.length = 0
   window.removeEventListener('keydown',moveDodger)
 
 
